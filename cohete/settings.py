530f6dc2e8ec52073937e840b647b35f355b42d9
yolanda <yolandithagarxiia@gmail.com>
2026-06-23 20:20:01 -0600

    cambio

--- cohete/settings.py
+++ cohete/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n^9suzfuhm7s8#rfr@n4g6v^h+ro1d_%-k8*=+q+7qk(r66ka9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cohete-nine.vercel.app', '.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
