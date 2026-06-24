fa02c5f0d7f3904eff0c17d79fe3e464a6e88abd
yolanda <yolandithagarxiia@gmail.com>
2026-06-23 20:11:50 -0600

    first commit

---
+++ cohete/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cohete project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cohete.settings')
+
+application = get_wsgi_application()
